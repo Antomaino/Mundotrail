43a4aa89420f8d8cef0ca9211cbacc6c55369e47
AntoMaino <antomaino93@gmail.com>
2023-05-31 21:18:34 -0300

    js clase 1

---
+++ js/app.js
@@ -0,0 +1,10 @@
+///CLASE 1 - ALGORITMO SIMPLE
+
+let nombre = "Maria";
+let apellido = "Lopez" ;
+let edad "40 añios";
+
+let resultadoA = nombre + apellido;
+let resultadoB = nombre + apellido + edad;
+
+console.log(resultadoA)
